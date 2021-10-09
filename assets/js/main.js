5c1b13a2b91817e54592d3c55605c6f8f0c7a2af
DanhNT99 <danh.nta.59cntt@ntu.edu.vn>
2021-10-09 21:34:30 +0700

    code slider by library slick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,37 @@
-const $ = document.querySelector.bind(document)
-
-const nodeBar = $('.icon-bar')
-const menu = $('.menu')
-const nodeClose = $('.menu-icon')
+$(document).ready(function () {
 const body = $('body')
-nodeBar.onclick = function() {
-    menu.classList.add('menu-show')
-}
-nodeClose.onclick = function () {
-    menu.classList.remove('menu-show')
-}
-window.onclick = function (e) {
+$('.icon-bar').click(function () {
+    $('.menu').addClass('menu-show')
+})
+$('.menu-icon').click( function () {
+    $('.menu').removeClass('menu-show')
+})
+$(window).click(function (e) {
     if(!e.target.closest('.menu') && !e.target.closest('.icon-bar')) {
-        menu.classList.remove('menu-show')
+        $('.menu').removeClass('menu-show')
     }
-}
-window.onload = function() {
+})
+// $(window).load(function() {
+//     setBgSizeForBody ()
+// })
+$(window).resize(function () { 
     setBgSizeForBody ()
-}
-window.onresize = function() {
-    setBgSizeForBody ()
-}
+});
 function setBgSizeForBody () {
-    let heightHeader = $('.header').offsetHeight
-    let heightHero = $('.hero').offsetHeight
-    body.style.backgroundSize=`100% ${heightHeader + heightHero}px`
+    let heightHeader = $('.header').height()
+    let heightHero = $('.hero').height()
+    $('body').css({'backgroundSize': `100% ${heightHeader + heightHero}px`})
 }
+    
+$('.project-blogs').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    speed: 500,
+    responsive: [
+        { breakpoint: 1023, settings: { slidesToShow: 2 }},
+        {  breakpoint: 767,  settings: { slidesToShow: 1 } }
+    ]
+  });
+});
